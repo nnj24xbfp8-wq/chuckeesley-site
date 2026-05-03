a5d7b2ba584d8ebc3b2e01b318a76ffbf60e6ea5
nnj24xbfp8-wq <nnj24xbfp8@privaterelay.appleid.com>
2026-05-03 14:15:29 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Chuck Eesley';
+export const SITE_DESCRIPTION = 'Professor of Management Science & Engineering at Stanford. Research on entrepreneurship, institutions, and innovation.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
