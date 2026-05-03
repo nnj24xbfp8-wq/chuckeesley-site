b4517f187b9a16780dbcb6e05eeb01736b2149b4
nnj24xbfp8-wq <nnj24xbfp8@privaterelay.appleid.com>
2026-05-03 14:28:11 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Chuck Eesley';
-export const SITE_DESCRIPTION = 'Professor of Management Science & Engineering at Stanford. Research on entrepreneurship, institutions, and innovation.';
+export const SITE_DESCRIPTION = 'Professor of Management Science & Engineering at Stanford. Co-Director for International Entrepreneurship at the Stanford Technology Ventures Program (STVP). Research on entrepreneurship, institutions, and innovation.';
+
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
